a4c3675341ef64df56fc745ba99facbf15345367
Whitney Krape <whitney@formfunction.io>
2026-08-27 21:51:25 -0400

    Reallllly minor.

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,9 @@ import { useState, useEffect, useRef } from 'react';
 
 const arrayOfQuestions = [
   { id: 1, questionText: "Thank You", correctAnswer: "ありがとうございます" },
-  { id: 2, questionText: "You're Welcome", correctAnswer: "おはよう" },
-  { id: 3, questionText: "Good Morning", correctAnswer: "おはようございます" }
+  { id: 2, questionText: "You're Welcome", correctAnswer: "どういたしまして" },
+  { id: 3, questionText: "Good Morning", correctAnswer: "おはようございます" },
+  { id: 4, questionText: "I'm home!", correctAnswer: "ただいま" }
 ];
 
 export default function Home() {  
@@ -39,6 +40,12 @@ export default function Home() {
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+          Which language are you working on?
+          Japanese, French, Spanish, German
+
+          What would you like to work on? 
+
+          Vocabulary, Grammar, Symbols
         </p>
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
 
